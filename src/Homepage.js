ba433e57c69e756481218652df44332a1c45a485
unknown <gbebejunior@gmail.com>
2022-03-18 06:47:44 -0700

    feature signup/login

---
+++ src/Homepage.js
@@ -0,0 +1,21 @@
+import './index.css';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+
+const Homepage = () =>{
+
+    return (
+            <>
+                <p className='text-blue-500'> Helloworld </p>
+                <SignUp/>
+                <Login/>
+            
+            
+            </>
+
+
+    )
+
+}
+
+export default Homepage
---
+++ src/pages/ClientDashboard.js
@@ -0,0 +1,31 @@
+
+
+const ClientDashboard = () =>{
+
+return(
+
+    <>
+        <div className="grid grid-cols-2">
+
+            <div className="grid grid-rows-4 md:grid-cols-2 xl:grid-cols-3 gap-10 p-20">
+                <div className="grid grid-rows-2"> Name </div>
+                <div className="grid grid-rows-2"> Address </div>
+                <div className="grid grid-rows-2"> State </div>
+                <div className="grid grid-rows-2"> Area </div>
+                <div className="grid grid-rows-2"> Lagos </div>
+
+            </div>
+            <div className="grid grid-cols-8 md:grid-cols-2 xl:grid-cols-3 gap-10 p-20">
+
+                    Paragraph
+            </div>
+        </div>
+        
+    
+    </>
+
+)
+
+}
+
+export default ClientDashboard
